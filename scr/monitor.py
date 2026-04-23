823c16c60955b6ed8d84cf2fae5e26816b7409e9
Samuel Braz <samuel.brazz@gmail.com>
2026-04-23 12:11:53 -0300

    Função de buscar elemento.

--- scr/monitor.py
+++ scr/monitor.py
@@ -31,11 +31,36 @@ class Monitor():
         return driver
     
 
+    def buscar_elemento(self):
+        iframes = self._driver.find_elements(By.TAG_NAME, "iframe")
+        print(f"Total de iframes: {len(iframes)}")
+
+        for i, iframe in enumerate(iframes):
+            try:
+                self._driver.switch_to.frame(iframe)
+                spans = self._driver.find_elements(
+                    By.XPATH,
+                    f"//span[contains(text(), '{self.item_buscar}')]"
+                )
+
+                if spans:
+                    print(f"✅ Encontrado no iframe {i}!")
+                    for s in spans:
+                        print(f"  -> '{s.text}'")
+                    break
+                else:
+                    self._driver.switch_to.default_content()
+
+            except Exception as e:
+                print(f"Erro no iframe {i}: {e}")
+                self._driver.switch_to.default_content()
+
 
     def iniciar(self):
         self._driver = self._criar_driver()
         self._driver.get(self.url)
-        time.sleep(30)  # aguarda carregamento inicia
+        time.sleep(8)  # aguarda carregamento inicia
+        self.buscar_elemento()
 
 
 
@@ -46,7 +71,7 @@ if __name__ == '__main__':
 
     monitor = Monitor(
         url="https://b3.com.br/pt_br/para-voce",
-        item_buscar="dolar",
+        item_buscar="Cogna Educacao S.A",
         on_mudanca=ao_mudar,
         )
     monitor.iniciar()
